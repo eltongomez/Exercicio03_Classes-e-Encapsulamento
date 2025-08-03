2b95d98521faed9f09c627cb5149e9e98848f113
eltongomez <eltongslima@hotmail.com>
2025-08-03 12:21:43 -0300

    feat: recharging water and shampoo

--- ex-03/src/BathMachine.java
+++ ex-03/src/BathMachine.java
@@ -26,4 +26,30 @@ public class BathMachine {
         shampoo -= 2;
         System.out.println(pet.getName() + " is now clean.");
     }
+
+    public void addWater() {
+        if (water == 30){
+            System.out.println("O reservatório de água está completo. Total: " + water);
+        } else {
+            water += 2;
+            System.out.println("Adicionado 2 litros de água. Total: " + water);
+        }
+    }
+
+    public void addShampoo() {
+        if (shampoo == 10){
+            System.out.println("O reservatório de shampoo está completo. Total: " + shampoo);
+        } else {
+            shampoo += 2;
+            System.out.println("Adicionado 2 litros de shampoo. Total: " + shampoo);
+        }
+    }
+
+    public int getWater() {
+        return water;
+    }
+
+    public int getShampoo() {
+        return shampoo;
+    }
 }
